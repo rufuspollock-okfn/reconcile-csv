d3b8c0a80ef52a620c17bed5d82b05303168f1cc
Michael Bauer <mihi@lo-res.org>
2013-12-03 17:16:50 +0300

    changed license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject reconcile-csv "0.1.1"
   :description "A Reconciliation service, connecting CSV's with OpenRefine"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "http://okfnlabs.org/reconcile-csv"
+  :license {:name "BSD 2-Clause"
+            :file "LICENSE" }
   :dependencies [[org.clojure/clojure "1.5.1"]
     [org.clojure/tools.nrepl "0.2.3"]
     [ring/ring-core "1.2.0"]
